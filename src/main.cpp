fe4b3188273d8eb66d118cc832da3bb4f7a33a78
Nicolas Schurando <nicolas@sitronlabs.com>
2023-12-24 02:28:29 +0100

    Added mechanism to wait for a controller or user defined value before starting.

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,13 @@
 #include <stdlib.h>
 
 /* */
-static double volatile m_value;  //!< In cubic meters
-static uint32_t m_request_time;
-static bool volatile m_response_received;
+static double volatile m_value;                 //!< In cubic meters
+static bool volatile m_value_initial_received;  //!<
 
 /* List of virtual sensors */
 enum {
-    SENSOR_0_GAS,  // S_GAS (V_VOLUME)
+    SENSOR_0_MANAL,  // S_INFO (V_TEXT)
+    SENSOR_1_GAS,    // S_GAS (V_VOLUME)
 };
 
 /**
@@ -56,8 +56,14 @@ void presentation(void) {
                 }
                 break;
             }
-            case SENSOR_0_GAS: {
-                if (present(SENSOR_0_GAS, S_GAS, F("Consommation")) == true) {
+            case SENSOR_0_MANAL: {
+                if (present(SENSOR_0_MANAL, S_INFO, F("Relevé manuel")) == true) {
+                    step++;
+                }
+                break;
+            }
+            case SENSOR_1_GAS: {
+                if (present(SENSOR_1_GAS, S_GAS, F("Consommation")) == true) {
                     step++;
                 }
                 break;
@@ -78,10 +84,18 @@ void presentation(void) {
  */
 void receive(const MyMessage &message) {
 
+    /* Handle user manual input initial value */
+    if (message.sensor == SENSOR_0_MANAL && message.getType() == V_TEXT) {
+        m_value = strtod(message.getString(), NULL);
+        m_value_initial_received = true;
+        MyMessage message(SENSOR_0_MANAL, V_TEXT);
+        send(message.set(""));
+    }
+
     /* Handle value sent by controller */
-    if (message.sensor == SENSOR_0_GAS && message.getType() == V_VOLUME) {
+    else if (message.sensor == SENSOR_1_GAS && message.getType() == V_VOLUME) {
         m_value = message.getFloat();
-        m_response_received = true;
+        m_value_initial_received = true;
     }
 }
 
@@ -99,41 +113,55 @@ void loop(void) {
             STATE_2,
             STATE_3,
             STATE_4,
+            STATE_5,
         } m_sm;
         switch (m_sm) {
 
             case STATE_0: {
 
-                /* Request last value
-                 * this is done to keep the value incrementing across reboots
-                 * the response will be processed in the receive() function */
-                request(SENSOR_0_GAS, V_VOLUME);
+                /* Present user with a text input to set the initial value */
+                MyMessage message(SENSOR_0_MANAL, V_TEXT);
+                if (send(message.set("")) != true) {
+                    sleep(1000);
+                    break;
+                }
 
                 /* Move on */
-                m_request_time = millis();
-                m_response_received = false;
                 m_sm = STATE_1;
                 break;
             }
 
             case STATE_1: {
 
-                /* Wait for value to be retrieved */
-                if (m_response_received == true) {
-                    Serial.printf(" [w] Retrieved value from controller.");
-                    m_sm = STATE_2;
+                /* Request last value
+                 * this is done to keep the value incrementing across reboots
+                 * the response will be processed in the receive() function */
+                if (request(SENSOR_1_GAS, V_VOLUME) != true) {
+                    sleep(1000);
+                    break;
                 }
 
-                /* Watch for timeout */
-                else if (millis() - m_request_time >= 5000) {
-                    Serial.println(" [w] Timed out when retrieving value from controller, starting from 0.");
-                    m_value = 0;
-                    m_sm = STATE_2;
-                }
+                /* Move on */
+                m_sm = STATE_2;
                 break;
             }
 
             case STATE_2: {
+
+                /* Wait for initial value to be retrieved */
+                if (m_value_initial_received != true) {
+                    sleep(1000, true);
+                    m_sm = STATE_1;
+                    break;
+                }
+
+                /* Move on */
+                Serial.printf(" [w] Retrieved value from controller.\r\n");
+                m_sm = STATE_3;
+                break;
+            }
+
+            case STATE_3: {
 
                 /* Compute battery level */
                 double adc_voltage = (1.1 * analogRead(CONFIG_BATTERY_PIN)) / 1023.0;
@@ -157,11 +185,11 @@ void loop(void) {
                 }
 
                 /* Move on */
-                m_sm = STATE_3;
+                m_sm = STATE_4;
                 break;
             }
 
-            case STATE_3: {
+            case STATE_4: {
 
                 /* Sleep until we get interrupted by either a pulse signal or a timeout */
                 uint8_t interrupt = digitalPinToInterrupt(CONFIG_GAZPAR_PULSE_PIN);
@@ -171,24 +199,24 @@ void loop(void) {
                  * increment the value and send the new one */
                 if (res == interrupt) {
                     m_value += 0.010;
-                    m_sm = STATE_4;
+                    m_sm = STATE_5;
                 }
 
                 /* If we have been interrupted by a timeout,
                  * just go back to the battery reporting part */
                 else if (res == MY_WAKE_UP_BY_TIMER) {
-                    m_sm = STATE_2;
+                    m_sm = STATE_3;
                 }
                 break;
             }
 
-            case STATE_4: {
+            case STATE_5: {
 
                 /* Send the value,
                  * but if it doesn't work don't bother retrying */
                 static uint32_t value_last = 0;
                 if (m_value != value_last) {
-                    MyMessage message(SENSOR_0_GAS, V_VOLUME);
+                    MyMessage message(SENSOR_1_GAS, V_VOLUME);
                     if (send(message.set((float)m_value, 3)) == true) {
                         value_last = m_value;
                     } else {
@@ -197,7 +225,7 @@ void loop(void) {
                 }
 
                 /* Move on */
-                m_sm = STATE_2;
+                m_sm = STATE_3;
                 break;
             }
         }
